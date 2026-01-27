35b02c9701d2e4c54310ea0251f9d6aa58adc3a6
victor7553 <victorhugo7553@gmail.com>
2026-01-26 21:58:43 -0300

    estrutura de pastas e arquivos e instalção da biblioteca tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PageComponent  } from './components/shared/page/page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
